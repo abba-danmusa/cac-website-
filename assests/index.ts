8ecacd83f2c0a14dbf9c977512e9980391f26769
Hafeez <hafeezabubakar15@gmail.com>
2025-02-26 19:55:06 +0100

    first commit

---
+++ assests/index.ts
@@ -0,0 +1,50 @@
+import clip from './Clip path group.png'
+import group from './Group 48.png'
+import like from './Facebook Like.png'
+import adminster from './Administrative Tools.png'
+import morale  from './Morale.png'
+import effort from './Effort.png'
+import pdf from './PDF.png'
+import clip2 from './Clip2.png'
+import youtube from './YouTube.png'
+import ellipse from './Ellipse 5.png'
+import home from './Home.svg'
+import map from './map.svg'
+import rg from './rg1.png'
+import tools1 from './tools1.svg'
+import tools2 from './tools2.svg'
+import cacd1 from './cacd1.png'
+
+import hand1 from './hand1.svg'
+import hand2 from './hand2.svg'
+import build1 from './build1.svg'
+import build2 from './biuld2.svg'
+import people1 from './people1.svg'
+import people2 from './people2.svg'
+import servicesSVG from './Services.svg'
+
+import person1 from './person1.svg'
+import person2 from './person2.svg'
+
+export {
+    servicesSVG,
+    clip,
+    group,
+    like,
+    adminster,
+    morale,
+    effort,
+    pdf,
+    clip2,
+    youtube,
+    ellipse,
+    home,
+    map, rg, tools1, hand1, hand2, tools2,
+    build1,
+    build2,
+    people1,
+    people2,
+    person1,
+    person2,
+    cacd1,
+}
